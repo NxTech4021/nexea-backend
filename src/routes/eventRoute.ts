722c20c1cab5e8d7ab839c5ed6438b3229d50a2a
Zawad Wasik Ahmed <zawadwasik@gmail.com>
2024-05-02 18:55:04 +0800

    Adding Delete Function to Events

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -1,9 +1,10 @@
-import { createEvent, getAllEvents, getEvent, updateEvent } from '@controllers/eventController';
+import { createEvent, deleteEvent, getAllEvents, getEvent, updateEvent } from '@controllers/eventController';
 import { Router } from 'express';
 
 export const eventRouter = Router();
 
 eventRouter.route('/create').post(createEvent);
 eventRouter.route('/events').get(getAllEvents);
+eventRouter.route('/delete/:id').delete(deleteEvent);
 eventRouter.route('/update/:id').put(updateEvent);
 eventRouter.route('/:id').get(getEvent);
